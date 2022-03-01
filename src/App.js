39f40982e9cced846d9e9b6ad48c6d8b12bf8522
vinay-1729 <vinaymurakala001@gmail.com>
2022-03-02 01:11:26 +0530

    login 02-03

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <div className="">
-        {/* <LoginPage /> */}
-      <PatientsList />
+        <LoginPage />
+      {/* <PatientsList /> */}
       </div>
     </div>
   );
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,88 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom';  
-import background from "../img/login-banner.png";  
+import React from "react";
+
 
 export default function LoginPage() {
   return (
-		<div className="container">
-			<div className="flex justify-center px-6 my-12 p-4">
-				<div className="w-full xl:w-3/4 lg:w-11/12 flex">
-					
-					<div className="m-4 w-full h-auto hidden lg:block lg:w-5/12 bg-cover rounded-l-lg">
-                        <div
-                            className="w-full h-auto hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
-                            style={{backgroundImage: `url(${background})`,width:600,height:400}}
-                        >
-                    </div>
-
-                    </div>
-					<div className="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
-						<div className="container w-10/12 shadow-md border rounded-lg border-bg-gray-100 p-3">
-                            <h3 className="pt-4 text-3xl text-gray-800 text-center text-left mr-16">Sign into your account</h3>
-                            <br />  
-                            <form className="px-8 pt-2 pb-8 mb-4 bg-white rounded">
-                                <div className="mb-6 md:flex md:justify-between">
-                                    <input type="email" placeholder="EMAIL" className="border border-gray-300 rounded text-sm py-3 px-2 w-full" />
-                                </div>
-                                <div className="mb-5 md:flex md:justify-between">
-                                    <input type="password" placeholder="***********" className="border border-gray-300 rounded text-sm py-3 px-2 w-full" />
-                                </div>
-                                <div className="mb-2">
-                                    {/* <Link    className='text-gray-500'> */}
-                                    <div className="text-gray-400 font text-md flex flex-row-reverse">
-                                    Forgot Password?
-                                    </div>
-                                    
-                                    {/* </Link> */}
-                                </div>
-                                
-                                
-                                {/* <div className="mb-4">
-                                    <label className="block mb-2 text-sm font-bold text-gray-700" for="email">
-                                        Email
-                                    </label>
-                                    <input
-                                        className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                        id="email"
-                                        type="email"
-                                        placeholder="Email"
-                                    />
-                                </div> */}
-                                
-                                <div className="mb-4 text-center">
-                                    <button
-                                        className="w-full px-4 text-xl text-white bg-green-600  rounded-lg py-2 hover:bg-green-500 focus:outline-none focus:shadow-outline"
-                                        type="button"
-                                    >
-                                        Login
-                                    </button>
-                                </div>
-                                <div className="flex justify-items-center">
-                                    <hr className="mb-6 w-5/12 border-t mt-3"/> 
-                                    <span className="w-2/12 text-gray-300">or</span> 
-                                    <hr className="mt-3 w-6/12 mb-6 border-t" />
-                                </div> 
-                                
-                                <div class="">
-								<div class="flex  space-x-2 justify-center items-end bg-gray-700 hover:bg-gray-600 text-white py-2 rounded-md transition duration-100">
-
-                                    < img class="h-5 cursor-pointer " src="https://i.imgur.com/arC60SB.png" alt="" />
-									<button >sign-in with google</button>
-								</div>
-							</div>
-                                <div className="text-center">
-                                    <a
-                                        className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-                                        href="./index.html"
-                                    >
-                                        Already have an account? Login!
-                                    </a>
-                                </div>
-                            </form>
-                        </div>
-					</div>
-				</div>
-			</div>
-		</div>
-  )
+      <div className="flex mx-48 my-16 bg-teal-200 h-[576px] shadow-lg">
+        <div className="w-2/6">
+        </div>
+        <div className="w-4/6 bg-white rounded-tl-[50px] rounded-bl-[50px] border-2	">
+          <div className="text-right text-zinc-500	mr-4 mt-3">
+            English(UK)
+          </div>
+          <div className="mx-16">
+          <h3 className="ml-2 mt-4 text-3xl font-semibold text-left">Login Form</h3>
+          <div className="flex">
+              <button role="button" className="flex border border-gray-300  text-sm py-3 px-2 w-full mx-3  mt-6 rounded-[15px]">
+                        < img class="ml-5 h-5 cursor-pointer " src="https://i.imgur.com/arC60SB.png" alt="" />
+                        <p class="text-base font-sm ml-4 text-gray-600">Continue with Google</p>
+                </button>
+                <button role="button" className="flex border border-gray-300  text-sm py-3 px-2 w-full mx-3  mt-6 rounded-[15px]">
+                        < img class="ml-5 h-5 cursor-pointer " src="https://i.imgur.com/p0LvxkX.png" alt="" />
+                        <p class="text-base font-sm ml-4 text-gray-600">Continue with Google</p>
+                </button>
+          </div>
+          <div className="flex mt-2 justify-items-center">
+              <hr className="mb-6 w-5/12 border-t mt-3"/> 
+              <span className="w-2/12 text-gray-500">OR</span>
+              <hr className="mt-3 w-5/12 mb-6 border-t" />
+          </div> 
+            <form action="">
+            <div className="mb-6 md:flex md:justify-between">
+                <input type="email" placeholder="Email Address" className="border-b-2 rounded text-sm py-3 px-2 w-full" />
+            </div>
+            <div className="mb-5 md:flex md:justify-between">
+                <input type="password" placeholder="Password" className="border-b-2  rounded text-sm py-3 px-2 w-full" />
+            </div>
+            </form>  
+            <div className="flex-row-reverse text-right pr-3 text-gray-500">
+                <a href="#" className="text-teal-400">Forgot Password?</a>
+            </div>
+            <button role="button" className="bg-teal-500 py-2 px-2 w-full mx-3 mt-2 rounded-[20px]">
+                    <p class="font-bold	 text-lg ml-4 text-white">Login</p>
+            </button>
+            <div className="flex text-right pr-3 ml-12 mt-4 text-gray-500">
+                <p>Don't have an Account?</p><a href="#" className="ml-2 text-teal-400">Sign Up</a>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+  );
 }
