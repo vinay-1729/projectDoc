604fdd2f0d402014d119d98d3f8489c77ea8f4a2
vinay-1729 <vinaymurakala001@gmail.com>
2022-03-08 22:52:53 +0530

    hh

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 // import LoginPage from "../src/components/LoginPage";
 // import SignupPage from "../src/components/SignupPage";
 // import PatientsList from "./components/PatientsList";
-import HomePage from "../src/components/HomePage";
+// import HomePage from "../src/components/HomePage";
+import PatientDashboard from "../src/components/PatientDashboard"
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
         {/* <LoginPage /> */}
       {/* <SignupPage /> */}
       {/* <PatientsList /> */}
-      <HomePage />
+      {/* <HomePage /> */}
+      <PatientDashboard />
     </div>
   );
 }
