e99cb2304b7b41bb24160ae548168c5fc7992b61
vinay-1729 <vinaymurakala001@gmail.com>
2022-03-09 09:28:02 +0530

    pp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-// import LoginPage from "../src/components/LoginPage";
+import LoginPage from "../src/components/LoginPage";
 // import SignupPage from "../src/components/SignupPage";
 // import PatientsList from "./components/PatientsList";
 // import HomePage from "../src/components/HomePage";
 import PatientDashboard from "./components/Dashboard/PatientDashboard"
-import Dashboard from "../src/components/Dashboard/Dashboard"
+// import Dashboard from "../src/components/Dashboard/Dashboard"
 
 function App() {
   return (
     <div className="App container mx-auto">
-        {/* <LoginPage /> */}
+        <LoginPage />
       {/* <SignupPage /> */}
       {/* <PatientsList /> */}
       {/* <HomePage /> */}
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import {AiOutlineEye ,AiOutlineEyeInvisible} from 'react-icons/ai'
 
 export default function LoginPage() {
+  const [state,setState] = useState(false);
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleBtn = () => {
+    setState(prevState => !prevState)
+  }
+    const togglePage = (index) => {
+        setToggleState(index);
+    };
   return (
     <div className="flex justify-center md:my-12 lg:my-12">
       <div className="flex w-full xl:w-7/12 lg:w-7/12 bg-teal-200 h-[576px] shadow-lg">
@@ -12,16 +21,18 @@ export default function LoginPage() {
             English (UK)
           </div>
           <div className="mx-6 lg:mx-16">
-          <h3 className="ml-2 mt-4 text-1xl md:text-3xl font-semibold md:text-left text-center">Login Form</h3>
+          <h3 className="ml-2 mt-4 text-1xl md:text-3xl font-semibold md:text-left text-center">
+            {(toggleState == 1) ? "Login Form" : "Register Form"}
+          </h3>
           <div className="lg:flex">
-              <button role="button" className="border border-gray-300 w-3/4 lg:w-full mt-6 rounded-[15px]">
+              <button role="button" className="border border-gray-300 w-3/4 lg:w-full mt-6 rounded-[15px] hover:bg-slate-100">
                 <div className="flex mx-2 my-2 justify-center">
                 < img class=" h-5 cursor-pointer " src="https://i.imgur.com/arC60SB.png" alt="" />
                 <p class="text-md px-2 text-gray-600">Continue with Google</p>
                 </div>
                 </button>
 
-                <button role="button" className="border border-gray-300 w-3/4 lg:w-full lg:ml-2 mt-6 rounded-[15px]">
+                <button role="button" className="border border-gray-300 w-3/4 lg:w-full lg:ml-2 mt-6 rounded-[15px] hover:bg-slate-100">
                 <div className="flex mx-2 my-2 justify-center">
                 < img class=" h-5 cursor-pointer " src="https://i.imgur.com/p0LvxkX.png" alt="" />
                 <p class="text-md px-2 text-gray-600">Continue with Facebook</p>
@@ -35,13 +46,16 @@ export default function LoginPage() {
           </div> 
             <form action="">
             <div className="relative mb-6 md:flex md:justify-between">
-                <input id="email" type="email" placeholder="Email Address" className="border-b-2 peer rounded text-sm py-3 px-2 w-full placeholder-transparent  outline-none" />
-                <label for="email" class="absolute left-2 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:sm peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
+                <input id="email" type="email" placeholder="Email Address" className="border-b-2 cursor-text peer rounded text-sm py-3 px-2 w-full placeholder-transparent  outline-none" />
+                <label for="email" class="absolute left-2 -top-3.5 text-gray-600 cursor-text text-sm transition-all peer-placeholder-shown:sm peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
 
             </div>
             <div className="relative mb-5 md:flex md:justify-between">
-                <input id="password" type="password" placeholder="Password" className="border-b-2 peer rounded text-sm py-3 px-2 w-full placeholder-transparent outline-none" />
-                <label for="password" class="absolute left-2 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:sm peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                <input id="password" type={(state)?"text":"password"} placeholder="Password" className="border-b-2 cursor-text peer rounded text-sm py-3 px-2 w-full placeholder-transparent outline-none" />
+                <label for="password" class="absolute left-2 -top-3.5 text-gray-600 cursor-text text-sm transition-all peer-placeholder-shown:sm peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                {!state? <AiOutlineEye className="bottom-3 cursor-pointer right-0 absolute" onClick={toggleBtn} /> : 
+                    <AiOutlineEyeInvisible className="bottom-3 cursor-pointer right-0 absolute" onClick={toggleBtn} />
+                }
             </div>
             </form>  
             <div className="justify-end text-right pr-3 text-gray-500">
@@ -53,7 +67,12 @@ export default function LoginPage() {
             </button>
             </div>
             <div className="flex text-right pr-3 ml-12 mt-4 text-gray-500">
-                <p>Don't have an Account?</p><a href="#" className="ml-2 text-teal-400">Sign Up</a>
+            <p>Don't have an Account?</p><a href="#" className="ml-2 text-teal-400" onClick={() => togglePage(1)}>Sign Up</a>
+              {/* {(toggleState == 1) ?
+               :
+              <p>Already have an Account?</p><a href="#" className="ml-2 text-teal-400" onClick={() => toggleTab(2)}>Login</a>
+              } */}
+                
             </div>
           </div>
           
