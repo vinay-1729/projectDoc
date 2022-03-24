70593900554cfe7f3aec124228e59782dc6b4e5f
vinay-1729 <vinaymurakala001@gmail.com>
2022-03-24 22:16:17 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import "./App.css";
-// import LoginPage from "./components/Login/LoginPage";
-// import SignupPage from "./components/Login/SignupPage";
-// import PatientsList from "./components/PatientsList";
-// import HomePage from "../src/components/HomePage";
-import PatientDashboard from "./components/Patient/PatientDashboard"
-import Prescription from "./components/Patient/AddPrescription.jsx"
-// import ChatBody from "./components/Chat/components/chatBody/ChatBody"
-
 function App() {
   return (
-    <div className="App container mx-auto">
-        {/* <LoginPage /> */}
-      {/* <SignupPage /> */}
-      {/* <PatientsList /> */}
-      {/* <HomePage /> */}
-      {/* <PatientDashboard /> */}
-      <Dashboard />
-      {/* <ChatBody /> */}
-      {/* <Prescription /> */}
+    <div className="container mx-auto">
+        Hello World
     </div>
   );
 }
--- src/components/subComponents/menu.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const menu = () => {
-  return (
-    <div>menu</div>
-  )
-}
-
-export default menu
